bc90af8a252c49d56f2fa7545f658a9503b29478
Jatin Yadav <jatinyadav5975@gmail.com>
2026-09-17 16:31:02 +0000

    conversion of datatypes

---
+++ basic/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score = "100"
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"100"=>100
+//"100abc"=>NaN
+//true=>1 false=>0
+
+let isLoggedIn = "0"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//""=>false "1"=>true "0"=>true(because it is non empty string)
+
+let someNumber = 123
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
