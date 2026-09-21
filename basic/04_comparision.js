9fa4a75eb4dff0ab9bb0ccfe04a37047f49c55b3
Jatin Yadav <jatinyadav5975@gmail.com>
2026-09-21 14:16:17 +0000

    comparision

---
+++ basic/04_comparision.js
@@ -0,0 +1,10 @@
+console.log("2">1) 
+
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//try to avoid these types of comparisions 
+
+console.log("2"==2);
