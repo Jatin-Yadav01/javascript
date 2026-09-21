94155a5551839c071258abbfc3331e69679e404e
Jatin Yadav <jatinyadav5975@gmail.com>
2026-09-21 13:57:44 +0000

    03_conversionOperations.js

--- basic/03_conversionOperations.js
+++ basic/03_conversionOperations.js
@@ -19,5 +19,20 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 123
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// *********************Operations********************
+
+let value=4;
+let negValue=-value;
+console.log(negValue)
+
+let str1="Jatin"
+let str2=" Yadav"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2)     //12
+console.log("1"+2+2)   //122 (bcz string + num=string)
+console.log(1+2+"2")   //32  (num+num=num then string)
